94365f924cd84574637078851a9d5bfb0d05fb1e
Dru Edmondson <peryton6@gmail.com>
2018-07-16 21:47:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,8 +29,8 @@ function currentLine(katzDeli){
   }
 
 function redDeliWheel(counter){
+ var dailyCount = 0;
   for (let i=0; i<100; i++){
-  var dailyCount = 
   dailyCount += 1;}
   return dailyCount;
 }
